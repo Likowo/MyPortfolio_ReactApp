d7f0bf8ffe2eb1219a6b88e1c17e74b862110767
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-02 23:43:35 -0500

    created link to my  linkedIn profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,17 @@
 // Upload/Create component boilerplate using rfce
-import { Link } from 'react-router-dom';
-import React from 'react'
+import { Link } from "react-router-dom";
+import React from "react";
 
 function Footer() {
   return (
-    <div className='footer'>
-      <Link to="/linkedIn">
+    <div className="footer">
+      <a href="https://www.linkedin.com/in/ann-likowo-molinge-39576473">
         <div>LinkedIn</div>
-      </Link>
-      <Link to="/calendy">
-        <a href="https://calendly.com/amolinge17"> 
-        <div >Calendy</div>
-        </a>
-      </Link>
-      <Link to="/github">
-        <div>GitHub</div>
-      </Link>
+      </a>
+
+    
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
