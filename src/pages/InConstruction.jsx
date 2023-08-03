0d693a25da3e75f33e2733e3fd90eb542e995913
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-03 00:48:42 -0500

    looking for week10 folder

--- src/pages/InConstruction.jsx
+++ src/pages/InConstruction.jsx
@@ -1,3 +1,8 @@
 export default function WorkInContruction (props){
     return <h1>This is the InConstruction page </h1>
 };
+
+//Import all paintings
+// import  from AbstactPainting './'
+
+{/* <img src="" alt="" /> */}
