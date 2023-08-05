720015baf1bc80870f14ddef6677381da65bd769
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-05 08:41:08 -0500

    Finally succeeded to link a downloadable  resume pdf to app. check out the simple and quick use of an anchor tag

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,17 +1,37 @@
 import React from "react";
+// import "./App.css";
+
+// Use this quick and simple way using anchor tag, to link to local pdf file (e.g Resume,cover letter) in a create-react-app project.
+
+<a
+  href="\pdfname.pdf"
+  download
+>
+  Download Resume
+</a>
 
 function Resume() {
   return (
     <div className="resumePage">
-      <div>
-        <h3> This is the Resume page </h3>
-      </div>
 
-      <div className="resumePdf">
-        <button> Download Resume</button>
-      </div>
+<div className="downLoadResume" >
+
+         <a href="\Resume  Portfolio.pdf" download >
+            Download Resume
+        </a>
+       </div>
+
     </div>
   );
 }
 
 export default Resume;
+
+///////////////////////////////////////
+{
+  /* <div className="contactLinks" >
+         <a href=" file:///C:/Users/user/OneDrive/Desktop/PerScholas/Interview%20Prep%20-Resources%20Software%20Engineering/Resumes%20and%20cover%20letters/Portfolio%20Project/Resume.pdf">
+        <div> Resume</div>
+        </a>
+       </div> */
+}
